1343de9ee4721370aa019de22c233ddfd08f91bd
Jason O'Neil <jason.oneil@gmail.com>
2023-02-04 14:40:09 +0800

    Set up Cinzel as serif font in TW

--- 2023/tailwind.config.js
+++ 2023/tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["_site/**/*.html"],
   safelist: [],
   theme: {
+    fontFamily: {
+      serif: ["Cinzel", ...defaultTheme.fontFamily.serif],
+    },
     extend: {
       colors: {
         change: "transparent",
